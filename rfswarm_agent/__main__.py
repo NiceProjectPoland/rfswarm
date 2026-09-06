7d0854e1f4ec0cb2c7c7431b028380f9ec2ce903
Arkadiusz Kuczynski <arkadiusz.kuczynski@niceproject.eu>
2026-09-06 15:25:35 +0200

    Deprecate xmlmode completely

--- rfswarm_agent/__main__.py
+++ rfswarm_agent/__main__.py
@@ -9,7 +9,6 @@ def configuration():
 	parser.add_argument('-m', '--manager', help='The manager to connect to e.g. http://localhost:8138/')
 	parser.add_argument('-d', '--agentdir', help='The directory the agent should use for files')
 	parser.add_argument('-r', '--robot', help='The robot framework executable')
-	parser.add_argument('-x', '--xmlmode', help='XML Mode, fall back to pasing the output.xml after each iteration', action='store_true')
 	parser.add_argument('-a', '--agentname', help='Set agent name')
 	parser.add_argument('-p', '--property', help='Add a custom property, if multiple properties are required use this argument for each property e.g. -p property1 -p "Property 2"', action='append')
 	parser.add_argument('-c', '--create', help='ICON : Create application icon / shortcut')
